45f96b84de75e1f8cfec25de7301da324650a391
bashye <michal.lepkowski2003@gmail.com>
2024-11-10 14:26:06 +0100

    Dodano funkcje czy_pierwsza do sprawdzenia, czy liczba jest pierwsza

--- projekt.cpp
+++ projekt.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
 
+bool czy_pierwsza(int liczba) {
+    if (liczba <= 1) return false;
+    for (int i = 2; i * i <= liczba; ++i) {
+        if (liczba % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
     int liczba;
     cin >> liczba;
-    cout << "Wprowadzona liczba: " << liczba << endl;
+    cout << "Wprowadzona liczba: " << liczba;
+    if (czy_pierwsza(liczba)) {
+        cout << " jest liczb¹ pierwsz¹." << endl;
+    } else {
+        cout << " nie jest liczb¹ pierwsz¹." << endl;
+    }
     return 0;
 }
